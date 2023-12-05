4e536cfe6200c5b9c814cbe853e434c8b484d5e1
weichslgartner <weichslgartner@gmail.com>
2023-12-05 13:39:58 +0100

    day05 part2

--- Python/day_05.py
+++ Python/day_05.py
@@ -20,6 +20,9 @@ class Interval():
     def __repr__(self):
         return f'-{self.begin} {self.end}-'
 
+    def length(self):
+        return self.end - self.begin + 1
+
 
 def parse_input(input):
     all_maps = []
@@ -41,80 +44,84 @@ def parse_input(input):
                 translation_maps.append(CMap(begin=ints[1], end=ints[1] + ints[2] - 1, target=ints[0]))
     translation_maps.sort(key=lambda x: x.begin)
     all_maps.append(translation_maps)
-
     return seeds, all_maps
 
 
-def solve(seeds, all_maps):
+def part_1(seeds, all_maps):
     best = sys.maxsize
     for seed in seeds:
         location = seed
         for stage in all_maps:
             location = binary_search(location, stage)
-        # print(location)
         best = min(location, best)
-       # print(location)
     return best
 
 
-def part_1(seeds, all_maps):
-    return solve(seeds, all_maps)
-
-
-def convert(location, maps: List[CMap]):
-    for cmap in maps:
-        if cmap.begin <= location <= cmap.end:
-            return cmap.target + location - cmap.begin
-    return location
-
-
-def convert2(interval: Interval, maps: List[CMap]):
+def convert_rages(interval: Interval, maps: List[CMap]):
     target = []
-    init_interval = Interval(interval.begin,interval.end)
-    # if interval.begin == 635790399:
-    #     print("debug")
-    n_seeds = interval.end - interval.begin
+    init_interval = Interval(interval.begin, interval.end)
+    n_seeds = interval.end - interval.begin + 1
+    trace_buf = []
     for i, cmap in enumerate(maps):
         next_cmap = maps[i + 1] if i < len(maps) - 1 else None
         assert interval.begin <= interval.end
-        # left interval overlaps or complete overlap
         if cmap.end >= interval.begin >= cmap.begin:
             if cmap.end >= interval.end:
                 target.append(
                     Interval(cmap.target + interval.begin - cmap.begin, cmap.target + interval.end - cmap.begin))
-                break
+                trace_buf.append("case 1")
+                interval.end = 0
+                interval.begin = 1
+                assert interval.length() + sum(t.length() for t in
+                                               target) == n_seeds, f"got {interval.length() + sum(t.length() for t in target)} expected {n_seeds}"
+                return target
             else:
                 target.append(Interval(cmap.target + interval.begin - cmap.begin, cmap.target + cmap.end - cmap.begin))
                 interval.begin = cmap.end + 1
+                assert interval.length() + sum(t.length() for t in
+                                               target) == n_seeds, f"got {interval.length() + sum(t.length() for t in target)} {n_seeds}"
+                trace_buf.append("case 2")
+
         # right side overlaps
         elif cmap.begin <= interval.end <= cmap.end:
-            target.append(Interval(cmap.target + cmap.begin - cmap.begin, cmap.target + interval.end - cmap.begin))
+            target.append(Interval(cmap.target, cmap.target + interval.end - cmap.begin))
             target.append(Interval(interval.begin, cmap.begin - 1))
-            break
+            interval.end = 0
+            interval.begin = 1
+            assert interval.length() + sum(t.length() for t in
+                                           target) == n_seeds, f"got {interval.length() + sum(t.length() for t in target)} {n_seeds}"
 
+            trace_buf.append("case 3")
+            return target
+        # cmap is included in interval
         elif cmap.begin >= interval.begin and cmap.end <= interval.end:
             target.append(Interval(cmap.target, cmap.target + cmap.end - cmap.begin))
-            if interval.begin <= cmap.begin-1:
-                target.append(Interval(interval.begin,cmap.begin-1))
-            if cmap.end+1 <= interval.end:
-                interval=Interval(cmap.end+1,interval.end)
-            else:
-                break
+            trace_buf.append("case 4")
+
+            if interval.begin < cmap.begin:
+                target.append(Interval(interval.begin, cmap.begin - 1))
+                interval = Interval(cmap.end + 1, interval.end)
+                trace_buf.append("case 5")
+            assert interval.length() + sum(t.length() for t in target) == n_seeds
+
+
         # no overlap
         elif next_cmap is not None:
             if interval.end < next_cmap.begin:
                 target.append(interval)
-                break
+                trace_buf.append("case 7")
+                return target
         else:
             target.append(interval)
-            break
+            trace_buf.append("case 7")
+            return target
+    target.append(interval)
     target.sort(key=lambda x: x.begin)
-    tareget_sum = sum(i.end-i.begin for i in target)
-    #if tareget_sum!=n_seeds:
-       # print(tareget_sum,n_seeds,init_interval)
-        #assert tareget_sum==n_seeds
+    tareget_sum = sum(t.length() for t in target)
+    if tareget_sum != n_seeds:
+        print(target, tareget_sum, n_seeds, init_interval, trace_buf, interval, len(maps))
+        assert tareget_sum == n_seeds
     if len(target) == 0:
-        #print("len(target)")
         return [interval]
     return target
 
@@ -134,33 +141,16 @@ def binary_search(location, maps: List[CMap]):
     return location
 
 
-def get_all_seeds(seeds_raw):
-    for begin, n in chunk(seeds_raw, 2):
-        for seed in range(begin, begin + n):
-            yield seed
-
-
 def part_2(seeds_raw, all_maps):
     best = sys.maxsize
-    for seed_begin,width in chunk(seeds_raw, 2):
-        intervals = [Interval(seed_begin, seed_begin+width-1)]
-        for i,stage in enumerate(all_maps):
+    for seed_begin, width in chunk(seeds_raw, 2):
+        intervals = [Interval(seed_begin, seed_begin + width - 1)]
+        for i, stage in enumerate(all_maps):
             new_intervals = []
-
             for interval in intervals:
-                tmp = convert2(interval,stage)
-                #if tmp[0].begin == 0:
-                #    print(i, tmp, interval.begin,debug_interval.begin)
-                new_intervals += tmp
-
-            #new_intervals.sort(key=lambda x: x.begin)
-            #print(i,new_intervals)
-
+                new_intervals += convert_rages(interval, stage)
             intervals = new_intervals
-           # print(i, intervals)
-        # print(location)
-        best = min(min(intervals,key=lambda x : x.begin).begin,best)
-        #print(Interval(seed_begin, seed_begin+width-1),best)
+        best = min(min(intervals, key=lambda x: x.begin).begin, best)
     return best
 
 
@@ -168,7 +158,7 @@ def main():
     lines = input_as_str("input_05.txt")
     seeds, all_maps = parse_input(lines)
     print("Part 1:", part_1(seeds, all_maps))
-    print("Part 2:", part_2( seeds,all_maps))
+    print("Part 2:", part_2(seeds, all_maps))
 
 
 if __name__ == '__main__':
