153cd56e09d07475341a8332d9ff4ca23c670115
weichslgartner <weichslgartner@gmail.com>
2023-12-08 18:43:10 +0100

    day 07 python refactor

--- Python/day_07.py
+++ Python/day_07.py
@@ -1,13 +1,13 @@
 from collections import Counter, namedtuple
 from enum import IntEnum
 from functools import cmp_to_key
-from typing import List
+from typing import List, Dict, Callable
 
 from aoc import get_lines
 
 
 class Hand(namedtuple('Hand', 'hand hand_count bid')):
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.hand} {self.hand_count} {self.bid}'
 
 
@@ -32,15 +32,12 @@ def parse_input(lines: List[str]) -> List[Hand]:
     return [Hand(hand, Counter(hand), int(bid)) for hand, bid in map(str.split, lines)]
 
 
-def get_highest(hand: Counter, part2=False) -> int:
+def get_highest(hand: Counter, part2: bool = False) -> HandRank:
     m = hand.most_common(2)
     most_com = m[0][1]
     sec_com = m[1][1] if len(m) > 1 else 0
     if 'J' in hand.keys() and part2:
-        for mc in hand.most_common():
-            if mc[0] != 'J':
-                most_com = mc[1] + hand['J']
-                break
+        most_com = substitute_jokers(hand, most_com)
     assert most_com <= 5
     match most_com, sec_com:
         case 5, _:
@@ -59,6 +56,13 @@ def get_highest(hand: Counter, part2=False) -> int:
             return HandRank.HIGHEST_CARD
 
 
+def substitute_jokers(hand: Hand, most_com) -> int:
+    for mc in hand.most_common():
+        if mc[0] != 'J':
+            return mc[1] + hand['J']
+    return most_com
+
+
 def cmp(c1: Hand, c2: Hand, part2: bool) -> int:
     val_1 = get_highest(c1.hand_count, part2)
     val_2 = get_highest(c2.hand_count, part2)
@@ -69,7 +73,7 @@ def cmp(c1: Hand, c2: Hand, part2: bool) -> int:
     return compare_by_card_part(c1, c2, order_dict1)
 
 
-def compare_by_card_part(c1: Hand, c2: Hand, order_dict):
+def compare_by_card_part(c1: Hand, c2: Hand, order_dict: Dict[str, int]) -> int:
     for i1, i2 in zip(c1.hand, c2.hand):
         if order_dict[i1] < order_dict[i2]:
             return -1
@@ -78,16 +82,16 @@ def compare_by_card_part(c1: Hand, c2: Hand, order_dict):
     return 0
 
 
-def total_winnings(hand_bids, cmp_fun):
+def total_winnings(hand_bids: List[Hand], cmp_fun: Callable[[Hand, Hand], int]) -> int:
     hand_bids.sort(key=cmp_to_key(cmp_fun))
     return sum((i + 1) * h.bid for i, h in enumerate(hand_bids))
 
 
-def part_1(hand_bids: List[Hand]):
+def part_1(hand_bids: List[Hand]) -> int:
     return total_winnings(hand_bids, lambda c1, c2: cmp(c1, c2, False))
 
 
-def part_2(hand_bids):
+def part_2(hand_bids: List[Hand]) -> int:
     return total_winnings(hand_bids, lambda c1, c2: cmp(c1, c2, True))
 
 
