5337ce10ff2b32e0d0309d084d263c0a9f984ef0
weichslgartner <weichslgartner@gmail.com>
2023-12-19 23:01:25 +0400

    day19 refactor

--- Python/day_19.py
+++ Python/day_19.py
@@ -1,41 +1,35 @@
-import string
-from collections import OrderedDict, defaultdict
-from copy import deepcopy
-from operator import gt, lt
-from typing import List
-from collections import Counter
-from aoc import get_lines, input_as_str
 import re
+import string
+from math import prod
+from operator import gt, lt
+from typing import List, Dict, Tuple
+
+from aoc import input_as_str
 
 cmp2fun = {"<": lt, ">": gt}
-
 cmp_negate = {">": "<", "<": ">"}
 cmp_negate_corr = {">": -1, "<": 1}
-
 min_max = {">": max, "<": min}
 
 
 class Rule:
-    def __init__(self, result, lhs=None, rhs=None, compare=None):
+    def __init__(self, result: str, lhs: str = None, rhs: int = None, compare: str = None):
         self.lhs = lhs
         self.rhs = rhs
         self.compare = compare
         self.result = result
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.lhs}  {self.compare} {self.rhs} -> {self.result}"
 
 
-def parse_input(in_str):
+def parse_input(in_str: str) -> Tuple[Dict[str, List[Rule]], List[Dict[str, int]]]:
     rules = dict()
     workflows_str, parts_str = in_str.split("\n\n", 1)
-    # px{a<2006:qkq,m>2090:A,rfg}
-    re.compile("( ([a-z])([<>])([a-z]+|[AR]))")
     for w in workflows_str.split("\n"):
         name, rest = w.split("{", 1)
         rules_group = []
         for group in rest.replace("}", '').split(","):
-
             if ">" in group:
                 r = extract_comparison(group, ">")
             elif "<" in group:
@@ -54,26 +48,18 @@ def parse_input(in_str):
     return rules, p_ratings
 
 
-def extract_comparison(group, comp=">"):
+def extract_comparison(group: str, comp: str = ">") -> Rule:
     subs = re.split(f'{comp}|:', group)
-    r = Rule(lhs=subs[0], rhs=int(subs[1]), compare=comp, result=subs[2])
-    return r
+    return Rule(lhs=subs[0], rhs=int(subs[1]), compare=comp, result=subs[2])
 
 
-def eval_rule(rule: Rule, pr):
+def eval_rule(rule: Rule, pr: Dict[str, int]) -> bool:
     if rule.lhs is None:
         return True
     return cmp2fun[rule.compare](pr[rule.lhs], rule.rhs)
 
 
-def part_1(rules: OrderedDict, p_ratings):
-    accepted = 0
-    for pr in p_ratings:
-        accepted += chain_eval(pr, rules)
-    return accepted
-
-
-def chain_eval(pr, rules):
+def chain_eval(pr: Dict[str, int], rules: Dict[str, List[Rule]]) -> int:
     cur_rules: List[Rule] = rules["in"]
     while True:
         for rule in cur_rules:
@@ -87,139 +73,39 @@ def chain_eval(pr, rules):
                     break
 
 
-def get_constraints(cur_key, reverse_list, rules, from_key, const, all_constraints, path):
-    invert_cmp = False
-    for r in reversed(rules[cur_key]):
-        if invert_cmp:
-            cmp_neg = cmp_negate[r.compare]
-            const[r.lhs + cmp_neg] = min_max[cmp_neg](const[r.lhs + cmp_neg], r.rhs)
-        if r.result == from_key and not invert_cmp:
-            if r.compare is not None:
-                const[r.lhs + r.compare] = min_max[r.compare](const[r.lhs + r.compare], r.rhs)
-            invert_cmp = True
-    if cur_key == "in":
-        if is_valid(const):
-            all_constraints.append(const)
-        path.reverse()
-      #  print(const,is_valid(const),comb_per_constr(const), path)
+def calc_combos(constraints: List[Dict[str, int]]) -> int:
+    return sum(comb_per_constr(const) for const in constraints)
+
+
+def comb_per_constr(const: Dict[str, int]) -> int:
+    return prod((const[c + "<"] - const[c + ">"] - 1) for c in "xmas")
+
+
+def forward_calc(cur_key: str, rules: Dict[str, List[Rule]], const: Dict[str, int],
+                 all_constraints: List[Dict[str, int]]) -> None:
+    if cur_key == "A":
+        all_constraints.append(const)
         return
-
-    for next_key in reverse_list[cur_key]:
-        tmp_const = const.copy()
-        tmp_path = path.copy()
-        tmp_path.append(cur_key)
-        get_constraints(next_key, reverse_list, rules, cur_key, tmp_const, all_constraints,tmp_path)
-
-
-def is_valid(const):
-    for c in "xmas":
-        if const[c+">"] >= const[c+"<"]:
-            return False
-    return True
-
-
-def overlap(new_range, old_range):
-    # left overlap
-    if old_range[0]<=new_range[0]<=old_range[1]:
-        return [new_range[0],min(old_range[1],new_range[1])]
-    elif old_range[0]<=new_range[1]<=old_range[1]:
-        return [max(old_range[0], new_range[0]),new_range[1]]
-    return []
-
-
-def find_new_combo(const, found):
-    to_remove = []
-    for f in found:
-        remove_overlap = True
-        ovs = {}
-        for c in "xmas":
-            ov = overlap([const[c+">"],const[c+"<"]],[f[c+">"],f[c+"<"]])
-            if len(ov) == 0:
-                ovs = {}
-                break
-            ovs[c+">"]=ov[0]
-            ovs[c+"<"]=ov[1]
-
-
-
-
-
-def calc_combos(constraints):
-    total_combos =0
-    for const in constraints:
-        total_combos += comb_per_constr(const)
-    return total_combos
-
-
-def comb_per_constr(const):
-    combos = 1
-    for c in "xmas":
-        combos *= (const[c + "<"] - const[c + ">"] - 1)
-    return combos
-
-def forward_calc(cur_key, rules, const, all_constraints, path):
-    if cur_key=="A":
-        path.append("A")
-        all_constraints.append(const.copy())
-        print(const,is_valid(const),comb_per_constr(const),path.copy())
+    if cur_key == "R":
         return
-    if cur_key == "R": return
-    if cur_key == "qqz":
-        print("debug")
-    print(cur_key,comb_per_constr(const),const)
     for r in rules[cur_key]:
         tmp_const = const.copy()
         if r.compare is not None:
             tmp_const[r.lhs + r.compare] = min_max[r.compare](tmp_const[r.lhs + r.compare], r.rhs)
             cmp_neg = cmp_negate[r.compare]
             const[r.lhs + cmp_neg] = min_max[cmp_neg](tmp_const[r.lhs + cmp_neg], r.rhs) + cmp_negate_corr[cmp_neg]
-        tmp_path = path.copy()
-        tmp_path.append(cur_key)
-        forward_calc(r.result, rules, deepcopy(tmp_const), all_constraints, tmp_path.copy())
+        forward_calc(r.result, rules, tmp_const, all_constraints)
 
 
+def part_1(rules: Dict[str, List[Rule]], p_ratings: List[Dict[str, int]]) -> int:
+    return sum(chain_eval(pr, rules) for pr in p_ratings)
 
-def part_2(rules):
+
+def part_2(rules: Dict[str, List[Rule]]) -> int:
     all_constraints = []
-    const = {"x>": 0, "x<": 4001,
-             "m>": 0, "m<": 4001,
-             "a>": 0, "a<": 4001,
-             "s>": 0, "s<": 4001
-             }
-    forward_calc("in", rules, const, all_constraints, [])
+    constraints = {f"{char}{comp}": value for char in "xmas" for comp, value in zip((">", "<"), (0, 4001))}
+    forward_calc("in", rules, constraints, all_constraints)
     return calc_combos(all_constraints)
-def part_2_old(rules):
-    constraints = []
-    reverse_list = reverse_graph(rules)
-    for k,v in reverse_list.items():
-        count = Counter(v)
-        if count.most_common()[0][1] > 1:
-            print(k,count.most_common() )
-
-    for r in reverse_list["A"]:
-        const = {"x>": 0, "x<": 4001,
-                 "m>": 0, "m<": 4001,
-                 "a>": 0, "a<": 4001,
-                 "s>": 0, "s<": 4001
-                 }
-        get_constraints(r, reverse_list, rules, 'A', const,constraints,["A"])
-
-    #for const in constraints:
-    #    print(const,is_valid(const))
-
-    #found = [const[0]]
-    #for const in constraints[1:]:
-    #    find_new_combo(const,found)
-
-    return calc_combos(constraints)
-
-
-def reverse_graph(rules):
-    reverse_list = defaultdict(list)
-    for key, val in rules.items():
-        for sr in val:
-            reverse_list[sr.result].append(key)
-    return reverse_list
 
 
 def main():
