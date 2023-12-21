c5ba1d7c9635467b15885251b3c7e6f8ec64b518
weichslgartner <weichslgartner@gmail.com>
2023-12-21 12:17:39 +0100

    day21 safety

--- Python/day_21.py
+++ Python/day_21.py
@@ -42,7 +42,7 @@ def is_rock(p, max_p):
 def part_2(rocks_row, start, max_p, steps=10):
     queue, next_queue = {start}, set()
     for i in range(steps):
-       # print(i, len(queue))
+        print(i,count_base(queue,max_p), len(queue))
         for p in queue:
             for n in [Point(p.x - 1, p.y), Point(p.x, p.y - 1), Point(p.x + 1, p.y), Point(p.x, p.y + 1)]:
                 if not is_rock(n, max_p):
@@ -60,13 +60,22 @@ def print_grid(max_p):
                 print(".", end="")
         print()
 
+
+def count_base(points, max_p):
+    cnt = 0
+    for p in points:
+        if (p.x // max_p.x) == 0 and  (p.y // max_p.y) == 0:
+            cnt+=1
+    return cnt
+
+
 def main():
-    lines = get_lines("input_21.txt")
+    lines = get_lines("input_21_test.txt")
     rocks, rocks_row, start, max_p = parse_input(lines)
     assert not is_rock(Point(-2, -1), max_p)
 
     print("Part 1:", part_1(rocks, start, max_p))
-    print("Part 2:", part_2(rocks_row, start, max_p))
+    print("Part 2:", part_2(rocks_row, start, max_p,100))
 
 
 if __name__ == '__main__':
