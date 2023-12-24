f76697cdc504a9ce07032867a27afefffd1dbf52
weichslgartner <weichslgartner@gmail.com>
2023-12-24 15:58:36 +0100

    day22 part2

--- Python/day_22.py
+++ Python/day_22.py
@@ -1,18 +1,6 @@
 from collections import defaultdict
-from copy import deepcopy
-from dataclasses import dataclass
-from typing import List, Tuple
 
 from aoc import get_lines, Point3
-
-
-# @dataclass
-# class Point3():
-#     x: int
-#     y: int
-#     z: int
-#     def __repr__(self):
-#         return f'{self.x},{self.y},{self.z}'
 
 
 def parse_input(lines):
@@ -20,57 +8,25 @@ def parse_input(lines):
     for line in lines:
         p1_str, p2_str = line.split("~", 1)
         shapes.append((Point3(*map(int, p1_str.split(",", 2))), Point3(*map(int, p2_str.split(",", 2)))))
-    print(shapes)
     return sorted(shapes, key=lambda x: min(x[0].z, x[1].z))
 
 
-def part_1(shapes: List[Tuple[Point3, Point3]]) -> int:
+def gen_shapes(shapes):
     max_x, max_y = max(shapes, key=lambda s: s[1].x)[1].x, max(shapes, key=lambda s: s[1].y)[1].y
     top = [[(0, -1) for x in range(0, max_x + 1)] for y in range(0, max_y + 1)]
     rests_list = defaultdict(set)
     for i, shape in enumerate(shapes):
         block_rests(rests_list, shape, top, i)
-        for y in range(0, max_y + 1):
-            for x in range(0, max_x + 1):
-                print(top[y][x], end="")
-            print()
-        print("===")
-    del rests_list[-1]
-    print(rests_list)
-
     supported_by = defaultdict(set)
-
     for k, vals in rests_list.items():
         for v in vals:
             supported_by[v].add(k)
-    print(supported_by)
     allshapes = {i for i in range(len(shapes))}
     critical_shapes = set()
     for v in supported_by.values():
         if len(v) == 1:
-            critical_shapes.add(v.pop())
-    part_a =  len(allshapes -critical_shapes)
-    overall = part_b(rests_list, supported_by, critical_shapes)
-    return  part_a, overall
-
-
-def part_b(rests_list, supported_by, critical_shapes):
-    overall = 0
-    print(critical_shapes)
-    for crit in critical_shapes:
-        queue = {crit}
-        next_q = set()
-        while len(queue) > 0:
-            to_remove = set()
-            for q in queue:
-                to_remove |= rests_list[q]
-            for r in to_remove:
-                if len(supported_by[r] - queue) == 0:
-                    overall +=1
-                    next_q.add(r)
-            queue,next_q = next_q,set()
-        print(overall)
-    return overall
+            critical_shapes.add(next(iter(v)))
+    return allshapes, critical_shapes, supported_by
 
 
 def block_rests(rest_set, shape, top, id_) -> bool:
@@ -104,16 +60,27 @@ def block_rests(rest_set, shape, top, id_) -> bool:
         rest_set[k] |= v
 
 
-def part_2(lines):
-    pass
+def part_1(allshapes, critical_shapes) -> int:
+    return len(allshapes - critical_shapes)
+
+
+def part_2(n_shapes, supported_by):
+    overall = 0
+    for i in range(n_shapes):
+        removed_bricks = set([i])
+        for j in range(i + 1, n_shapes):
+            if len(supported_by[j] - removed_bricks) == 0:
+                removed_bricks.add(j)
+                overall += 1
+    return overall
 
 
 def main():
-    lines = get_lines("input_22_test.txt")
+    lines = get_lines("input_22.txt")
     shapes = parse_input(lines)
-    p1,p2 = part_1(shapes)
-    print("Part 1:", p1)
-    print("Part 2:", p2) #82042 too low
+    allshapes, critical_shapes, supported_by = gen_shapes(shapes)
+    print("Part 1:", part_1(allshapes, critical_shapes))
+    print("Part 2:", part_2(len(shapes), supported_by))
 
 
 if __name__ == '__main__':
